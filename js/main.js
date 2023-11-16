f697116ae8e161a6fb443fe672b710aad2ea7430
rebs-code <jack.rebussi@gmail.com>
2023-11-16 19:48:19 +0100

    style: format JS

--- js/main.js
+++ js/main.js
@@ -157,36 +157,3 @@ let gameEnded = false;
 const playButton = document.getElementById("play");
 playButton.addEventListener("click", campoMinato);
 
-// playButton.addEventListener("click", function () {
-//   resetBoard();
-//   //predo il valore del select
-//   const inputValue = document.getElementById("difficolta").value;
-//   if (inputValue === "3") {
-//     for (let i = 1; i <= 49; i++) {
-//       const myElement = createElement("div", "cell-7", i);
-//       myElement.addEventListener("click", function () {
-//         console.log(`Cell ${i} clicked!`);
-//         myElement.classList.toggle("blue");
-//       });
-//       document.getElementById("board").append(myElement);
-//     }
-//   } else if (inputValue === "2") {
-//     for (let i = 1; i <= 81; i++) {
-//       const myElement = createElement("div", "cell-9", i);
-//       myElement.addEventListener("click", function () {
-//         console.log(`Cell ${i} clicked!`);
-//         myElement.classList.toggle("blue");
-//       });
-//       document.getElementById("board").append(myElement);
-//     }
-//   } else if (inputValue === "1") {
-//     for (let i = 1; i <= 100; i++) {
-//       const myElement = createElement("div", "cell-10", i);
-//       myElement.addEventListener("click", function () {
-//         console.log(`Cell ${i} clicked!`);
-//         myElement.classList.toggle("blue");
-//       });
-//       document.getElementById("board").append(myElement);
-//     }
-//   }
-// });
